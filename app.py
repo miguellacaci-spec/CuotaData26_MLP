90fb0213237f8d47d1b3d01c83d39ff3a19f2c37
miguellacaci-spec <miguel.lacaci@outlook.es>
2025-11-08 03:19:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,5 @@ def delete_player(id):
 
 
 if __name__ == "__main__":
-    init_db()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
